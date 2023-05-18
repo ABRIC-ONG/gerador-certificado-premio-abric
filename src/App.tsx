6cf492c7f2146295652303794020b52844a23f30
leoprietsch <leo.prietsch@gmail.com>
2023-05-17 21:44:46 -0300

    feat: add header with ABRIC's logo and link to website

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
+import logo from "./assets/logo.png";
 import "./App.css";
 
 function App() {
-  return <h1>ABRIC</h1>;
+  return (
+    <>
+      <header className="flex w-screen justify-center">
+        <div className="flex w-full max-w-7xl items-center justify-between p-2">
+          <img src={logo} alt="Logo da ABRIC" className="h-12" />
+          <a className="cursor-pointer">
+            <h1 className="text-2xl font-bold text-green-700">Site</h1>
+          </a>
+        </div>
+      </header>
+    </>
+  );
 }
 
 export default App;
